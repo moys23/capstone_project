7c66c7f49695bce4dc38e779fe1a86f16d1f0112
Romel Silo <romellacsa.silo@bicol-u.edu.ph>
2021-06-23 22:26:46 +0800

    Update index.php

--- UsersPage/index.php
+++ UsersPage/index.php
@@ -26,22 +26,61 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../font/bootstrap-icons.css">
     <script src="../js/cdn.jquery.min.js"></script>
+    <style>
+      [data-toggle="collapse"].collapsed .if-not-collapsed,
+      [data-toggle="collapse"]:not(.collapsed) .if-collapsed {display: none;}
+    </style>
   </head>
   <body>
 
     <?php include_once 'nav.php'; ?>
 
       <div class="d-flex justify-content-start">
-       <div class="p-2 w-25 flex-shrink-1">
-         <ul class="list-group">
-           <li class="list-group-items">Overview
-             
-           </li>
-           <li class="list-group-items">...</li>
-           <li class="list-group-items">...</li>
-           <li class="list-group-items">...</li>
-         </ul>
-       </div>
+       <div class=" w-25 flex-shrink-1">
+           <ul class="list-group">
+             <li class="list-group-item">
+               <a class="text-decoration-none text-dark m-0 collapsed" data-toggle="collapse" href="#collapseContent" role="button" aria-expanded="false" aria-controls="collapseContent">
+                 <span class="if-collapsed">Overview <i class="bi bi-chevron-left"></i></span>
+                 <span class="if-not-collapsed">Overview <i class="bi bi-chevron-down"></i></span>
+               </a>
+                  <div class="collapse  border-0" id="collapseContent">
+                    <ul class="list-group">
+                       <li class="list-group-item">
+
+
+                         <a class="text-decoration-none text-dark m-0 collapsed" data-toggle="collapse" href="#collapseContent1.1" role="button" aria-expanded="false" aria-controls="collapseContent1.1">
+                           <span class="if-collapsed">Overview <i class="bi bi-chevron-left"></i></span>
+                           <span class="if-not-collapsed">Overview <i class="bi bi-chevron-down"></i></span>
+                         </a>
+                            <div class="collapse" id="collapseContent1.1">
+                              <ul class="list-group">
+                                 <li class="list-group-item">
+
+                                 </li>
+                                 <li class="list-group-item">
+
+                                 </li>
+                                 <li class="list-group-item">
+
+                                 </li>
+                              </ul>
+                            </div>
+
+
+                       </li>
+                       <li class="list-group-item">
+
+                       </li>
+                       <li class="list-group-item">
+
+                       </li>
+                    </ul>
+                  </div>
+             </li>
+             <li class="list-group-item">...</li>
+             <li class="list-group-item">...</li>
+           </ul>
+         </div>
        <div class="p-2 w-100 bg-success">Flex item</div>
       </div>
 
