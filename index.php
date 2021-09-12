f30908295703a4c765123c7ffb0c9c7f3e3455a9
Romel Silo <romellacsa.silo@bicol-u.edu.ph>
2021-09-12 22:44:58 +0800

    zxxczcxczx

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="icon" href="#" type="image/x-icon">
+  <link rel="icon" href="images/fav.jpg" type="image/x-icon" sizes="16x16">
   <title>BUPC Supply Office Inventory System</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="css/bootstrap.css">
